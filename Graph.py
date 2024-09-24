a842f7e9876983e32f72937673f40a1b8a1bab34
sowmiyan0318 <asowmiyan1@gmail.com>
2024-09-24 18:41:55 +0530

    Add files via upload

---
+++ Graph.py
@@ -0,0 +1,30 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list:
+            self.adj_list[vertex] = []
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adj_list and vertex2 in self.adj_list:
+            self.adj_list[vertex1].append(vertex2)
+            self.adj_list[vertex2].append(vertex1)
+
+    def display_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, "->", self.adj_list[vertex])
+
+# Example usage:
+graph = Graph()
+graph.add_vertex("A")
+graph.add_vertex("B")
+graph.add_vertex("C")
+graph.add_vertex("D")
+
+graph.add_edge("A", "B")
+graph.add_edge("A", "C")
+graph.add_edge("B", "D")
+graph.add_edge("C", "D")
+
+graph.display_graph()
